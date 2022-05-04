d9b36e2cd273545c79c70b700ca5cacc7ca8c55c
Yjeese <103395726+Yjeese@users.noreply.github.com>
2022-05-04 10:08:17 +0800

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,50 @@
+var t = null;
+t = setTimeout(time, 1000); 
+function time() {
+  clearTimeout(t); 
+  dt = new Date();
+  var y = dt.getFullYear();
+  var mt = dt.getMonth() + 1;
+  var day = dt.getDate();
+  var h = dt.getHours(); 
+  var m = dt.getMinutes(); 
+  var s = dt.getSeconds(); 
+  document.querySelector(".showTime").innerHTML =
+    "Present time：" +
+    day +
+    "/" +
+    mt +
+    "/" +
+    y +
+    "---" +
+    h +
+    ":" +
+    m +
+    ":" +
+    s;
+  t = setTimeout(time, 1000); 
+} 
+ 
+function validateform(){  
+var name=document.myform.name.value;  
+var password=document.myform.password.value;  
+  
+if (name==null || name==""){  
+  alert("Name can't be blank");  
+  return false;  
+}else if(password.length<6){  
+  alert("Password must be at least 6 characters long.");  
+  return false;  
+  }  
+}  
+ 
+x = 9
+text = prompt ("please enter a value")
+
+
+if (text === "student") {
+  window.location.href = "https://yjeese.github.io/CV55/course.html";
+}else if (text === "parent"){
+window.location.href = "https://yjeese.github.io/CV55/index.html";
+}
+document.getElsementById("demo").innerHTML =" end script3";
